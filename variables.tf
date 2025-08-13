13dee4854e3a5fd0935eaaa6b9b6d0c275919d0d
derryman101 <derrynix@gmail.com>
2025-08-14 00:37:24 +0100

    Add default values for instance_ami and ssh_key_name

--- variables.tf
+++ variables.tf
@@ -7,11 +7,13 @@ variable "aws_region" {
 variable "instance_ami" {
   description = "AMI ID for the EC2 instance"
   type        = string
+  default     = "ami-0c02fb55956c7d316"  # Amazon Linux 2 in us-east-1
 }
 
 variable "ssh_key_name" {
   description = "Name of the SSH key pair to use for EC2 (without .pem)"
   type        = string
+  default     = "derry"
 }
 
 variable "instance_type" {
